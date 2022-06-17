8cb92d21ff3fe4f335003ced54e733da91bf9931
Mattis Kjellerås <mattis@gr8.site>
2022-06-17 23:13:07 +0200

    only singleplayer works :(

--- src/main/java/site/gr8/mattis/ThrowEnderPearls.java
+++ src/main/java/site/gr8/mattis/ThrowEnderPearls.java
@@ -3,27 +3,58 @@ package site.gr8.mattis;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
+import net.minecraft.item.Items;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.dedicated.DedicatedServer;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
 
 public class ThrowEnderPearls {
 
 	public static void tick(MinecraftClient client) {
 		while (Julkinen.throwEnderPearlsKey.wasPressed()) {
 			PlayerInventory inventory = client.player.getInventory();
-			for (int i = 0; i < inventory.size(); i++) {
+			if (client.isInSingleplayer()) {
+				MinecraftServer server = client.getServer();
+				ServerPlayerEntity player = server.getPlayerManager().getPlayer(client.player.getUuid());
+				dropEnderPearlsSinglePlayer(inventory, player);
+			}
+			else {
 
-				ItemStack stack = inventory.getStack(i);
-				String stackName = stack.getName().getString();
+				//  ------ not working !! ----------
+				//  server is null
+				//  --------------------------------
 
-				if (stackName.equalsIgnoreCase("ender pearl")) {
+				//MinecraftServer server = client.getServer();
+				//DedicatedServer dedicatedServer = (DedicatedServer) client.player.getServer();
+				//ServerPlayerEntity serverPlayer = server.getPlayerManager().getPlayer(client.player.getUuid());
+				//dropEnderPearlServer(inventory, serverPlayer);
+			}
 
-					client.player.dropItem(stack, false, false);
+		}
+	}
 
-					client.player.sendMessage(Text.of("Currently not able to throw ep"), true);
-					// debug message to show if this got triggered
-
-				}
+	private static void dropEnderPearlsSinglePlayer(PlayerInventory inventory, ServerPlayerEntity player) {
+		for (int i = 0; i < inventory.size(); i++) {
+			ItemStack stack = inventory.getStack(i);
+			if (stack.getItem() == Items.ENDER_PEARL) {
+				player.getInventory().removeStack(i);
+				player.dropItem(stack, false, false);
 			}
 		}
 	}
+
+	private static void dropEnderPearlServer(PlayerInventory inventory, ServerPlayerEntity player) {
+		for (int i = 0; i < inventory.size(); i++) {
+			ItemStack stack = inventory.getStack(i);
+			if (stack.getItem() == Items.ENDER_PEARL) {
+				player.getInventory().removeStack(i);
+				player.dropItem(stack, false, false);
+			}
+		}
+	}
+
+
 }
